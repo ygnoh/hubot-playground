5b20b8d6f0cfd0bda750c3b14058efda02e55854
yonggoonoh <nohyg90@gmail.com>
2018-06-16 14:37:55 +0900

    play with hear and respond method

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,11 @@
 module.exports = robot => {
-    robot.hear(/javascript/i, res => {
-        res.send("wow");
-    });
+  // hear: anytime a message's text matches
+  robot.hear(/javascript/i, res => {
+    res.send('wow');
+  });
+
+  // respond: when messages are immediately preceded by the robot’s name or alias
+  robot.respond(/hello/i, res => {
+    res.reply('hi');
+  });
 };
